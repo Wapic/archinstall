17792f16e0cff024ddaa16740a40738ef2db6c9e
Wapic <mail@linuslundqvist.se>
2024-10-24 19:22:49 +0200

    add FULL option to completely automate install

--- archinstall.sh
+++ archinstall.sh
@@ -1,10 +1,113 @@
 loadkeys sv-latin1
+timedatectl set-timezone Europe/Stockholm
 timedatectl set-ntp true
 
-echo "keymap and timedate set"
-read -p '[exit, partition, mount, chroot, root, user] ' continue
+echo 'keymap and timedate set'
+read -p '[exit, partition, mount, chroot, root, user, FULL] ' continue
 if [ $continue == "exit" ]; then
     echo 'exiting...'
+    exit 1
+fi
+
+if [ $continue == "FULL" ]; then
+    read -p 'Are you sure you want to continue? [y/N]' wipeWarning
+    if [ $wipeWarning != "y" ]; then
+        echo 'aborting...'
+        exit 1
+    fi
+
+    echo 'starting full install!'
+    read -p 'username: ' username
+    read -rs -p 'password: ' rootPassword
+    echo -ne '\n'
+    read -rs -p 'confirm password: ' rootPassword2
+    echo -ne '\n'
+    read -p 'is vm? [y/N]' isVM
+    read -p 'boot disk: ' bootDrive
+    read -p 'hostname: ' hostname
+    read -p 'gpu? [AMD/nvidia]' gpuDriver
+    
+    if [ $rootPassword != $rootPassword2 ]; then
+        echo 'passwords do not match!'
+        exit 1
+    fi
+
+    nvme=false
+    if [[ $bootDrive == *"nvme"* ]]; then
+        nvme=true
+    fi
+    
+    sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+    sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+
+    # Delete and create new partitions
+    umount -R /mnt
+    swapoff -a 
+    sfdisk --delete $bootDrive
+    echo -e 'size=1G, type=U\n size=8G, type=S\n size=+, type=L\n' | sfdisk $bootDrive
+    
+    # nvme partition names differ from other drives 
+    if [ $nvme == true ]; then
+        bootDrive=$bootDrive"p" 
+    fi
+
+    # Formatting
+    mkfs.fat -F 32 $bootDrive"1" # /dev/sda1 or /dev/nvme0n1p1
+    mkswap $bootDrive"2"
+    mkfs.ext4 $bootDrive"3"
+
+    # Mount drives
+    mount $bootDrive"3" /mnt
+    mount --mkdir $bootDrive"1" /mnt/boot
+    swapon $bootDrive"2"
+
+    #Install base packages
+    pacstrap /mnt base linux-zen dhcpcd iwd neovim vim sudo man-db man-pages texinfo base-devel refind
+    if [ $isVM == "y" ]; then
+        pacstrap /mnt intel-ucode qemu-guest-agent lemurs hyprland openssh kitty git zip unzip
+    else 
+        pacstrap /mnt linux-firmware amd-ucode lemurs hyprland wireplumber pipewire pipewire-alsa pipewire-pulse pipewire-audio pcmanfm playerctl fastfetch firefox kitty git openssh zip unzip jdk8-openjdk jdk21-openjdk dunst libnotify wofi waybar hyprpicker hyprlock hyprpaper hypridle imv wl-clipboard slurp grim qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk btop zsh fzf nautilus gnome-keyring nextcloud-client bat glxinfo
+    fi
+    
+    if [ $isVM != "y" ]; then 
+        if [ $gpuDriver == "nvidia" ]; then
+            pacstrap /mnt nvidia-dkms nvidia-settings lib32-nvidia-utils
+        else
+            pacstrap /mnt mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
+        fi
+    fi
+
+    genfstab -U /mnt > /mnt/etc/fstab
+    arch-chroot /mnt /bin/bash <<END
+        ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime &&
+        hwclock --systohc &&
+        sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen &&
+        locale-gen &&
+        echo LANG=en_US.UTF-8 > /etc/locale.conf &&
+        echo KEYMAP=sv-latin1 > /etc/vconsole.conf && 
+        echo $hostname > /etc/hostname &&
+        mkinitcpio -P &&
+        useradd $username -m -s /bin/bash -U -G wheel -p $rootPassword &&
+        echo $rootPassword | passwd $username --stdin &&
+        passwd -l root &&
+        refind-install &&
+        rm /boot/refind_linux.conf &&
+        sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf &&
+        sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf &&
+        sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers &&
+        pacman -Syu --noconfirm --needed &&
+        cd /home/$username/ &&
+        git clone https://aur.archlinux.org/yay.git/ &&
+        cd yay &&
+        makepkg -si &&
+        exit
+END
+    rootPartition=$bootDrive"3"
+    rootUUID=$(sudo blkid /dev/nvme0n1p2 | grep -Eo "UUID=\".{36}\"\s" | grep -Eo ".{36}\b")
+    echo -e '"Boot with standard options"  "ro root=UUID='$rootUUID'"\n"Boot to single-user mode"    "ro root=UUID='$rootUUID'   single"\n"Boot with minimal options"   "ro root=UUID='$rootUUID'"' > /mnt/boot/refind_linux.conf
+    umount -R /mnt
+    swapoff -a 
+    echo 'Install finished! please reboot and remove install media!'
     exit 1
 fi
 
@@ -33,7 +136,7 @@ if [ $continue == "mount" ]; then
     mount $rootPartition /mnt
     mount --mkdir $efi /mnt/boot
     swapon $swap
-    pacstrap /mnt base linux linux-firmware dhcpcd iwd neovim vi sudo
+    pacstrap /mnt base linux-zen linux-firmware dhcpcd iwd neovim vim sudo
     genfstab -U /mnt > /mnt/etc/fstab
     cp ./archinstall.sh /mnt/archinstall.sh
     echo 'SCRIPT FINISHED chroot into /mnt and run script again'
@@ -69,11 +172,9 @@ fi
 if [ $continue == "root" ]; then   
     echo 'Creating user...'
     read -p 'Username: ' username
-    useradd $username -m
+    useradd $username -m -G wheel
     echo 'Password: '
     passwd $username
-    echo 'Adding user to group: wheel'
-    usermod -aG wheel $username
     echo 'Finished! login to new user and run user script. remember to uncomment %wheel using visudo!'
     cp ./archinstall.sh /home/$username/
 fi
@@ -82,7 +183,7 @@ if [ $continue == "user" ]; then
     read -p 'What gpu driver? [amd/nvidia]' gpuDriver
    
     if [ $gpuDriver == "amd" ]; then
-        echo 'not yet implemented'
+        pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver
     fi
    
     if [ $gpuDriver == "nvidia" ]; then
@@ -96,30 +197,17 @@ if [ $continue == "user" ]; then
         fi
     fi
     
-    echo 'installing login manager...'
-    pacman -S lemurs
+    pacman -S lemurs wayland hyprland wofi waybar swaybg swayimg wl-clipboard slurp grim qt5-wayland qt6-wayland xdg-desktop-portal-hyprland
+    echo -e '#! /bin/sh\nexec Hyprland' > /etc/lemurs/wayland/hyprland
+    chmod 755 /etc/lemurs/wayland/hyprland
 
-    echo 'Wayland will install with Hyprland'
-    echo 'Xorg will install with i3-wm'
-    read -p 'Display server? [x/wayland]' displayServer
-    if [ $displayServer == "x" -o $displayServer == "xorg" ]; then
-        pacman -S xorg i3-wm rofi polybar feh xclip dunst
-    fi
-    
-    if [ $displayServer == "wayland" ]; then
-        pacman -S wayland hyprland wofi waybar swaybg swayimg wl-clipboard mako slurp grim qt5-wayland qt6-wayland xdg-desktop-portal-hyprland
-        echo -e '#! /bin/sh\nexec Hyprland' > /etc/lemurs/wayland/hyprland
-        chmod 755 /etc/lemurs/wayland/hyprland
-        echo -e '--ozone-platform-hint=auto' > .config/electron-flags.conf
-    fi
-
-    read -p 'Install laptop brightness control? [Y/N]' brtnessctl
+    read -p 'Install brightness control? [Y/N]' brtnessctl
     if [ $brtnessctl == "Y" -o $brtnessctl == "y" ]; then
         pacman -S brightnessctl
     fi
 
     echo 'installing essentials...'
-    pacman -S wireplumber pipewire pipewire-alsa pipewire-pulse pipewire-audio pcmanfm playerctl neofetch firefox kitty base-devel git openssh zip unzip jdk8-openjdk
+    pacman -S wireplumber pipewire pipewire-alsa pipewire-pulse pipewire-audio pcmanfm playerctl fastfetch firefox kitty base-devel git openssh zip unzip jdk8-openjdk jdk21-openjdk dunst libnotify
 
     read -p 'install yay? [Y/N]' yay
     if [ $yay == "Y" -o $yay == "y" ]; then
